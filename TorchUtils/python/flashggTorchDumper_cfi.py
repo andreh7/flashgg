d0ea92d6cdc7deed18dc5db920395de829913073
ah <ah>
2016-05-10 13:05:14 +0200

    added support for writing out rechits in sparse format: list of lists of rechits

--- TorchUtils/python/flashggTorchDumper_cfi.py
+++ TorchUtils/python/flashggTorchDumper_cfi.py
@@ -8,11 +8,13 @@ flashggTorchDumper = cms.EDAnalyzer('FlashggTorchDumper',
                                         output = cms.untracked.string("rechits-barrel.t7"),
                                         windowHalfWidth = cms.untracked.uint32(3),
                                         windowHalfHeight = cms.untracked.uint32(11),
+                                        writeSparse = cms.untracked.bool(True),
                                         ),
 
                                     endcap = cms.untracked.PSet(
                                         output = cms.untracked.string("rechits-endcap.t7"),
                                         windowHalfWidth = cms.untracked.uint32(3),
                                         windowHalfHeight = cms.untracked.uint32(11),
+                                        writeSparse = cms.untracked.bool(True),
                                         ),
                                    )
